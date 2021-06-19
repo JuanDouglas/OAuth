ea40f72a0acca6e306d2ce28f75c557378403f2b
Juan Douglas <juandouglas2004@gmail.com>
2021-06-19 00:22:55 -0300

    Add testing

--- Solution/OAuth.Api/Models/Result/Authentication.cs
+++ Solution/OAuth.Api/Models/Result/Authentication.cs
@@ -7,5 +7,21 @@ namespace OAuth.Api.Models.Result
 {
     public class Authentication
     {
+        public int ID { get; set; }
+        public string UserAgent { get; set; }
+        public string IPAdress { get; set; }
+        public string Token { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsValid { get; set; }
+        public Authentication() { }
+        public Authentication(Dal.Models.Authentication authentication)
+        {
+            ID = authentication.Id;
+            UserAgent = authentication.UserAgent;
+            IPAdress = authentication.Ipadress;
+            Token = authentication.Token;
+            Date = authentication.Date;
+            IsValid = authentication.IsValid;
+        }
     }
 }
