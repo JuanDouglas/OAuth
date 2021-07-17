51b52d446a6d1f320826a953dff9b38cfe8e1776
Juan Douglas <juandouglas2004@gmail.com>
2021-07-17 13:25:13 -0300

    Add error tool

---
+++ Solution/OAuth.Api/wwwroot/Resources/js/tools.js
@@ -0,0 +1,17 @@
+function enableError(inputID, error) {
+    var input = document.getElementById(inputID);
+    var labelError = document.getElementById(inputID + "Error");
+    input.classList.add('error');
+    labelError.innerText = error;
+}
+
+function disableError(inputID) {
+    var input = document.getElementById(inputID);
+    input.classList.remove('error');
+    labelError.innerText = null;
+}
+
+function enableOrDisableError(inputID, error) {
+    var input = document.getElementById(inputID);
+    
+}
