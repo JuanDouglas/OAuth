4510ea4b00d900572ee610dc6147f69dead66dfe
Juan Douglas <juandouglas2004@gmail.com>
2021-09-09 15:21:29 -0300

    Load save state

--- Solution/replace.bat
+++ Solution/replace.bat
@@ -1,7 +1,28 @@
-for /f "tokens=1,* delims=]" %%A in ('"type %1|find /n /v """') do (
-set "line=%%B"
-if defined line (
-    call set "line=echo.%%line:%~2=%~3%%"
-    for /f "delims=" %%X in ('"echo."%%line%%""') do %%~X
-    ) ELSE echo.
+move OAuth.Dal\Models\OAuthContext.cs OAuth.Dal
+SET Directory=%CD%\OAuth.Dal\OAuthContext.cs
+setlocal
+
+call :FindReplace "OAuth.Dal.Models" "OAuth.Dal" %Directory%
+call :FindReplace "using System" "using OAuth.Dal.Models" %Directory%
+
+exit /b 
+
+:FindReplace <findstr> <replstr> <file>
+set tmp="%temp%\tmp.txt"
+If not exist %temp%\_.vbs call :MakeReplace
+for /f "tokens=*" %%a in ('dir "%3" /s /b /a-d /on') do (
+  for /f "usebackq" %%b in (`Findstr /mic:"%~1" "%%a"`) do (
+    echo(&Echo Replacing "%~1" with "%~2" in file %%~nxa
+    <%%a cscript //nologo %temp%\_.vbs "%~1" "%~2">%tmp%
+    if exist %tmp% move /Y %tmp% "%%~dpnxa">nul
+  )
 )
+del %temp%\_.vbs
+exit /b
+
+:MakeReplace
+>%temp%\_.vbs echo with Wscript
+>>%temp%\_.vbs echo set args=.arguments
+>>%temp%\_.vbs echo .StdOut.Write _
+>>%temp%\_.vbs echo Replace(.StdIn.ReadAll,args(0),args(1),1,-1,1)
+>>%temp%\_.vbs echo end with
--- Solution/run.bat
+++ Solution/run.bat
@@ -24,36 +24,7 @@ dotnet add OAuth.Dal package Microsoft.EntityFrameworkCore.Design
 @echo Create Context
 dotnet ef dbcontext scaffold "Server=.\SQLEXPRESS;Database=OAuth;Trusted_Connection=true;" Microsoft.EntityFrameworkCore.SqlServer -o Models -p OAuth.Dal
 @echo Move context 
-move OAuth.Dal\Models\OAuthContext.cs OAuth.Dal
-SET Text=using OAuth.Dal.Models;
-SET Directory=%CD%\OAuth.Dal\OAuthContext.cs
-setlocal
-
-call :FindReplace "OAuth.Dal.Models" "OAuth.Dal" %Directory%
-call :FindReplace "using System" "using OAuth.Dal.Models" %Directory%
-
-exit /b 
-
-:FindReplace <findstr> <replstr> <file>
-set tmp="%temp%\tmp.txt"
-If not exist %temp%\_.vbs call :MakeReplace
-for /f "tokens=*" %%a in ('dir "%3" /s /b /a-d /on') do (
-  for /f "usebackq" %%b in (`Findstr /mic:"%~1" "%%a"`) do (
-    echo(&Echo Replacing "%~1" with "%~2" in file %%~nxa
-    <%%a cscript //nologo %temp%\_.vbs "%~1" "%~2">%tmp%
-    if exist %tmp% move /Y %tmp% "%%~dpnxa">nul
-  )
-)
-del %temp%\_.vbs
-exit /b
-
-:MakeReplace
->%temp%\_.vbs echo with Wscript
->>%temp%\_.vbs echo set args=.arguments
->>%temp%\_.vbs echo .StdOut.Write _
->>%temp%\_.vbs echo Replace(.StdIn.ReadAll,args(0),args(1),1,-1,1)
->>%temp%\_.vbs echo end with
-
+start replace.bat
 @echo Remove Surplus
 rm %CD%\OAuth.Dal\Class1.cs
 @echo Insert Defaults
