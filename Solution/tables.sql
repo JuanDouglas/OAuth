d24f94384a9e7324f49e37d35c126cedc1932462
Juan Douglas <juandouglas2004@gmail.com>
2021-09-09 01:51:02 -0300

    Update database

--- Solution/tables.sql
+++ Solution/tables.sql
@@ -31,7 +31,7 @@ CREATE TABLE [Company] (
     [CNPJ] VARCHAR(100) UNIQUE NOT NULL,
     [Icon] INTEGER NOT NULL,
     [Account] INTEGER UNIQUE NOT NULL,
-    FOREIGN KEY ([Account])  REFERENCES [Account]([ID])
+    FOREIGN KEY ([Account]) REFERENCES [Account]([ID]),
     FOREIGN KEY ([Icon]) REFERENCES [Image]([ID])
 );
 
@@ -129,15 +129,3 @@ CREATE TABLE [AccountConfirmation](
     FOREIGN KEY ([Account]) REFERENCES [Account]([ID])
 );
 
-/* INSERT IMAGES */
-INSERT INTO [Image]([FileName],[FileType]) VALUES ('default_profile_image.png',1);
-INSERT INTO [Image]([FileName],[FileType]) VALUES ('default_application_icon.png',2);
-INSERT INTO [Image]([FileName],[FileType]) VALUES ('default_company_icon.png',3);
-
-/* INSERT USERS */
-INSERT INTO [Account] ([UserName],[Password],[Key],[ZipCode],[Email],[PhoneNumber],[IsCompany],[AcceptTermsDate],[Valid],[CreateDate],[ProfileImageID]) 
-VALUES ('JuanDouglas','$2a$10$vl2ajEUyNi7/pGsMqy1N1.h65XLywq.FRwiX43t.58f2Ou1fYgSSu','pr1v@t3K31H4Sh','71881-663','juandouglas2004@gmail.com','+55 (61) 9 9260-6441',1,GETDATE(),1,GETDATE(),1);
-
-/* INSERT APPLICATIONS */
-INSERT INTO [Application] ([Name],[Owner],[Key],[Icon],[PrivateKey],[Site],[LoginRedirect],[AuthorizeRedirect]) 
-VALUES('Stock-Manager-Api',1,'Nexus-Stock-Manager',2,'Pr1v4T3K31H@sh3d','https://nexus-stock-manager.azurewebsites.net/','https://nexus-stock-manager.azurewebsites.net/api/OAuth/Login?authorization_token={authorization-token}&account_id={account-id}&login_token={login-token}','https://nexus-stock-manager.azurewebsites.net/api/OAuth/Authorize?authorization_token={authorization-token}&account_id={account-id}');
